0a3e49a6503f5838e5429169e6d131d94b244ab6
Skailodon <jackiehtruong@outlook.com>
2025-01-13 19:39:18 -0800

    Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,52 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main class="main">
+        <div class="flex flex-col appLayout">
+          <section>
+            <div
+              class="bg-red-500 h-4/5 col-start-1 col-end-6 rounded-tr-3xl w-full m-auto"
+            >
+              Hero Image
+            </div>
+            <div
+              class="bg-red-500 h-4/5 col-start-8 col-end-13 rounded-tl-3xl w-full m-auto"
+            >
+              About Me
+            </div>
+          </section>
+          <section>
+            <div class="col-start-4 col-end-10 flex flex-col">
+              <div class="bg-red-500 h-8 flex items-center justify-center">
+                <h1>Portfolio</h1>
+              </div>
+              <div class="bg-green-500 flex flex-wrap mt-6 justify-center">
+                <div class="bg-blue-500 h-72 w-72">component loop + 6 columns</div>
+                <div class="bg-blue-500 h-72 w-72">component loop + 6 columns</div>
+                <div class="bg-blue-500 h-72 w-72">component loop + 6 columns</div>
+                <div class="bg-blue-500 h-72 w-72">component loop + 6 columns</div>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div
+              class="bg-blue-500 h-full col-start-1 col-end-7 w-full flex items-center justify-center"
+            >
+              text Section
+            </div>
+            <div class="bg-red-500 h-full col-start-7 col-end-13 w-full m-auto">
+              text Section
+            </div>
+          </section>
+          <section>
+            <div
+              class="bg-red-500 h-full col-span-full w-full m-auto flex items-center justify-center"
+            >
+              <h1>Hello World</h1>
+            </div>
+          </section>
+        </div>
+      </main>
     </>
   )
 }
