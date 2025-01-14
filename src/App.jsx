d9c64ac2d007a367b5868ba7753bef38963b3591
Skailodon <jackiehtruong@outlook.com>
2025-01-13 20:10:45 -0800

    nav system

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Contact from "./Pages/Contact/Contact";
+import Project from "./Pages/Project/Project"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <main class="main">
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/Contact">Contact</Link>
+          <Link to="/Project">Project</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Project" element={<Project />} />
+        </Routes>
+      </Router>
+
+      {/* <main class="main">
         <div class="flex flex-col appLayout">
           <section>
             <div
@@ -53,7 +67,7 @@ function App() {
             </div>
           </section>
         </div>
-      </main>
+      </main> */}
     </>
   )
 }
