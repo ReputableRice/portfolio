118a32a25c736f2ec68ab10c80a12328c6d46ad9
Skailodon <jackiehtruong@outlook.com>
2025-02-05 11:25:05 -0800

    debugging exploration with Sonja

--- src/components/live2d.jsx
+++ src/components/live2d.jsx
@@ -2,28 +2,51 @@ import React, { useEffect } from 'react';
 import * as PIXI from 'pixi.js';
 import { Live2DModel } from 'pixi-live2d-display';
 
+import courier from "./courier/courier.model3.json"
+
+window.PIXI = PIXI
+
+Live2DModel.registerTicker(PIXI.Ticker)
+
 export default function Live2DComponent() {
+ 
   useEffect(() => {
     const app = new PIXI.Application({
       view: document.getElementById('canvas'),
       autoStart: true,
       resizeTo: window,
     })
+    
+    const fetchedModel = Live2DModel.fromSync(courier)
 
-    Live2DModel.from('/courier/courier.model3.json').then((model) => {
-      app.stage.addChild(model)
+    fetchedModel.once('load', () => {
+      
+        app.stage.addChild(fetchedModel);
 
-      model.anchor.set(0.5, 0.5)
-      model.position.set(window.innerWidth / 2, window.innerHeight / 2)
-      model.scale.set(0.1, 0.1)
-
-      model.on('hit', () => {
-        model.motion('Tap@Body')
+        model.anchor.set(0.5, 0.5);
+        model.position.set(window.innerWidth / 2, window.innerHeight / 2);
+        model.scale.set(0.1, 0.1);
       })
-    })
+    
+
   }, [])
 
+  // async function fetchModel(){
+  //   try {
+  //     debugger
+  //     const model = await Live2DModel.from("./courier/courier.model3.json")
 
-  return <canvas id="canvas" />
+  //     debugger
+    
+  //   } catch(error) {
+  //     console.log(error)
+  //   }
+  // }
+
+  return (
+    <>
+      <canvas id="canvas" />
+    </>
+  )
 };
 
